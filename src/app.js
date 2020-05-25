5912b8b5b5d0586547d78394ef4fb9a2968fc597
unknown <diokajr@gmail.com>
2020-05-26 00:02:38 +0100

    Bug fixes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
-
 const port = process.env.PORT || 3000
 
 //Define paths for Express config
@@ -110,6 +109,6 @@ app.get('*', (req, res) => {
 
 })
 
-app.listen(port,  () => {
-	console.log('Server is up on port 3000')
+app.listen(port, () => {
+	console.log('Server is up on port ' + port)
 })
